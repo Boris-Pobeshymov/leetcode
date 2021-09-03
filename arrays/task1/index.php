820e1a07340ce40f3c5b126e16a769bf56feb044
Boris <boris@mgroupweb.com>
2021-09-03 22:01:30 +0300

    we know native functions for this task)

--- arrays/task1/index.php
+++ arrays/task1/index.php
@@ -1,7 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: boris
- * Date: 2021-09-03
- * Time: 14:55
+ * @param Integer[] $nums
+ * @return Integer
+ */
+function removeDuplicates(&$nums) {
+    $nums  = array_unique($nums);
+}
+
+
+/*
+362 / 362 test cases passed.
+Status: Accepted
+Runtime: 137 ms
+Memory Usage: 17.5 MB
  */
